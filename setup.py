950911193190c9a96f1f6ac0c3c2b6ddd2ff143d
revival0728 <69998786+revival0728@users.noreply.github.com>
2022-12-13 09:50:27 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'catcode',
-    version = '0.3.0',
+    version = '1.0.0',
     author = 'revival0728',
     description = 'A repository that prints source code in terminal prettier',
     url = 'https://github.com/revival0728/catcode',
